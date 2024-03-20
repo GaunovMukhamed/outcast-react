d614f5ec37b94ab3035faf686f048e23586765c2
SPACERIDER <spacerider@example.com>
2024-03-20 22:06:49 +0300

    add toast

--- src/pages/login-page/login.service.ts
+++ src/pages/login-page/login.service.ts
@@ -5,4 +5,10 @@ const address: string = process.env.REACT_APP_API_ADDRESS!;
 
 export const loginUser = (login: string): Promise<SuccessResponse> => {
   return axios.post(address + '/login', { login })
+    .then((response) => {
+      return response.data;
+    })
+    .catch((error) => {
+      return error;
+    });
 }
